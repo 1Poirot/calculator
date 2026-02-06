5b1c35018628932d7a8f39f3c753ec3579e0b11f
Jakkapong <MinneJung5@gmail.com>
2026-02-06 18:58:49 +0700

    JJ

--- Calculator.java/Calculator.java
+++ Calculator.java/Calculator.java
@@ -1,50 +1,37 @@
 import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        public static void main(String[] args) {
+        System.out.print("Enter first number: ");
+        double total = sc.nextDouble();
 
-                Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter operator (+ - or =): ");
+            String operator = sc.next();
 
-                System.out.println("===CALCULATOR===");
-                System.out.println("[0] [1] [2] [3] [4]");
-                System.out.println("[5] [6] [7] [8] [9]");
-                System.out.println();
-                System.out.println("Operations: + -");
-                System.out.println("Press = to calculate");
-                System.out.println("====================");
+            
+            if (operator.equals("=")) {
+                break;
+            }
 
-                System.out.println("Enter first number: ");
-                double num1 = sc.nextDouble();
+            System.out.print("Enter next number: ");
+            double nextNum = sc.nextDouble();
 
-                System.out.println("Enter operator (+ or -):");
-                double op = sc.next().charAt(0);
-                
-                System.out.println("Enter second number:");
-                double num2 = sc.nextDouble();
-
-                double result = 0;
-                
-                if (op == '+') {
-                        result = num1 + num2;
-                } else if (op == '-') {
-                        result = num1 - num2;
-                } else {
-                        System.out.println("Invalid operator!");
-                        sc.close();
-                        return;
-                }
-
-                System.out.println();
-                System.out.println("Process");
-                System.out.println(num1 + " " + op + "" + num2);
-
-                System.out.println("Result:");
-                System.out.println(result);
-
-                sc.close();
-
-
-
-
+            
+            if (operator.equals("+")) {
+                total += nextNum;
+            } else if (operator.equals("-")) {
+                total -= nextNum;
+            } else {
+                System.out.println("Invalid operator!");
+            }
         }
+
+        System.out.println("====================");
+        System.out.println("Final Result: " + total);
+        
+        sc.close();
+    }
 }
